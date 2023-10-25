16e91b35bc29b767f4ab898ed4b2aca79b9f7bbd
Shubham Singh <shubham.s@zomato.com>
2023-10-25 12:07:01 +0530

    add print in worker

--- worker.py
+++ worker.py
@@ -1,22 +1,22 @@
+print("Starting worker...")
 import schedule
 import time
 
 from job import monitor_hint, build_report, crux, top_page_lcp, prefetches
 
-print("Starting worker...")
+tz = "Asia/Kolkata"
 
 # Schedule tasks
-schedule.every(1).day.at("11:00").do(top_page_lcp.run)
-schedule.every().day.at("01:00").do(monitor_hint.run)
-schedule.every().day.at("04:00").do(monitor_hint.run)
-# schedule.every().day.at("08:00").do(build_report.run)
-# schedule.every().day.at("08:10").do(crux.run)
-# schedule.every().day.at("10:00").do(prefetches.run)
-schedule.every(2).hours.do(prefetches.run)
-schedule.every(2).hours.do(build_report.run)
-schedule.every(2).hours.do(crux.run)
+schedule.every(1).day.at("01:00 PM", tz).do(top_page_lcp.run)
+schedule.every(1).day.at("03:00 PM", tz).do(monitor_hint.run)
+schedule.every(1).day.at("06:00 PM", tz).do(monitor_hint.run)
+schedule.every(1).day.at("09:00 PM", tz).do(build_report.run)
+schedule.every(1).day.at("12:10 PM", tz).do(crux.run)
+schedule.every(1).day.at("01:00 PM", tz).do(prefetches.run)
 
 while True:
     schedule.run_pending()
+    print("Sleeping for 5 minutes...")
     time.sleep(5 * 60) # 5 minutes
-    # time.sleep(30) # 30 seconds
+
+print("Worker stopped.")
