027dda6f034ed85408dd95b130373914d4b28c3d
Shubham Singh <shubham.s@zomato.com>
2023-10-26 09:17:31 +0530

    change cron timing

--- worker.py
+++ worker.py
@@ -7,12 +7,11 @@ from job import monitor_hint, build_report, crux, top_page_lcp, prefetches
 tz = "Asia/Kolkata"
 
 # Schedule tasks
-schedule.every(1).day.at("13:00", tz).do(top_page_lcp.run)
-schedule.every(1).day.at("15:00", tz).do(monitor_hint.run)
-schedule.every(1).day.at("18:00", tz).do(monitor_hint.run)
-schedule.every(1).day.at("21:00", tz).do(build_report.run)
-schedule.every(1).day.at("12:10", tz).do(crux.run)
-schedule.every(1).day.at("13:30", tz).do(prefetches.run)
+schedule.every(1).day.at("13:00", tz).do(monitor_hint.run)
+schedule.every(1).day.at("18:00", tz).do(build_report.run)
+schedule.every(1).day.at("19:00", tz).do(crux.run)
+schedule.every(1).day.at("20:00", tz).do(top_page_lcp.run)
+schedule.every(1).day.at("23:00", tz).do(prefetches.run)
 
 while True:
     schedule.run_pending()
